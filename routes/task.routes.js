8dc10b8eb90ca65ed6e10e9973980b897da6d81e
Lionel <Lionel5garcia@gmail.com>
2022-06-23 10:56:05 +0200

    feat: taskRoutes with verifying token and user

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const taskController = require("../contrellers/TaskController");
+const isSuperAdmin = require('../middlewares/isSuperAdmin');
+const verifyToken = require('../middlewares/verifyToken');
 
-router.get("/tasks", taskController.getAll);
-router.post("/tasks", taskController.create);
-router.put("/tasks/:id", taskController.update);
-router.delete("/tasks/:id", taskController.delete);
+router.get("/tasks",verifyToken, isSuperAdmin, taskController.getAll);
+router.post("/tasks", verifyToken, isSuperAdmin, taskController.create);
+router.put("/tasks/:id", verifyToken, isSuperAdmin, taskController.update);
+router.delete("/tasks/:id", verifyToken, isSuperAdmin, taskController.delete);
 
 module.exports = router;
